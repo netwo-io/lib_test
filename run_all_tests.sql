5ad6b23176be98d74da69a08b8626cd1c775af55
François-Guillaume Ribreau <github@fgribreau.com>
2020-09-29 18:02:24 +0200

    mob next [ci-skip]

--- run_all_tests.sql
+++ run_all_tests.sql
@@ -1,1 +1,4 @@
 select json_agg(row_to_json(results)) from lib_test.run_all() results;
+
+-- @todo upgrade CLI to select tests to run, see:
+-- select json_agg(row_to_json(results)) from lib_test.run_suite('lib_cloudinary') results;
